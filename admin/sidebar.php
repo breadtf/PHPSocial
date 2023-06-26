a4df26c1bab84d9e2def7623853db05a9515544d
BreadTeleporter <103989916+BreadTeleporter@users.noreply.github.com>
2023-06-26 10:44:29 +0100

    Update sidebar.php

--- admin/sidebar.php
+++ admin/sidebar.php
@@ -1,3 +1,10 @@
+<?php
+// If adminEnabled is false, redirect to home page
+// This is here because every admin page has the sidebar
+if ($configs["adminEnabled" === false){
+    header("Location: ../index.php");
+}
+?>
 <div class="admin-sidebar">
     <h1><a href="/"><?php echo $configs["sitename"] ?></a></h1>
     <a href="index.php">Message logs</a><br>
